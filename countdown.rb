1ece97b2d875a94ba3af02cd8862fd1f84ba2a7b
Fillan Lee <fillanlee@aol.com>
2019-02-03 15:06:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,12 @@ def countdown (number)
   
   while number > 0 do
     puts "#{number} SECOND(S)!"
+    countdown_with_sleep
     number -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep (pause)
-  
+  sleep(1)
 end
